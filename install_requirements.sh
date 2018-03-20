542e33e6b5fc4c7da6cbcaf0aad81536aba4ca10
greg_cervone <greg.cervone@macmillan.com>
2018-03-20 10:25:58 -0500

    added user to install requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,5 +4,5 @@
 
 echo 'installing required modules...'
 
-pip-3.6 install -r env_builder/requirements.txt
-pip-3.6 install -r deploy/requirements.txt
+pip-3.6 install -r env_builder/requirements.txt --user
+pip-3.6 install -r deploy/requirements.txt --user
