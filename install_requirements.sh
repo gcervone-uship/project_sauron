821f4b21dde19ec81b2d0b44c5001eeacbc27506
Taylor May <taylor.may.contractor@macmillan.com>
2018-04-03 16:27:45 -0500

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,6 +4,6 @@
 
 echo 'installing required modules...'
 
-pip-3.6 install -r env_builder/requirements.txt
-pip-3.6 install -r deploy/requirements.txt
-pip-3.6 install -r py_sauron/requirements.txt
+pip-3.6 install -r env_builder/requirements.txt --user
+pip-3.6 install -r deploy/requirements.txt --user
+pip-3.6 install -r py_sauron/requirements.txt --user
