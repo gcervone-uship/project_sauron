0af408aecccf175b35a8d1e085f8c055bb1c94c9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 20:13:19 +0000

    Adding tarfile member sanitization to extractall()

--- py_sauron/upload_to_s3.py
+++ py_sauron/upload_to_s3.py
@@ -61,7 +61,26 @@ def extract_tar(tarball_path, destination_action, subdirectory=''):
     """
     with TemporaryDirectory() as temp_dir, tarfile.open(tarball_path) as tarball:
         assembled_path = Path(temp_dir, subdirectory).expanduser()
-        tarball.extractall(path=temp_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarball, path=temp_dir)
         destination_action(assembled_path)
 
 def make_key_from_path(path_name, key_prefix=None):
